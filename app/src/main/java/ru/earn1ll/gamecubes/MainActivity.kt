5f4db19bd888a0b085e816b402d45dce92419d5e
Earn1ll <coldsteelblr@gmail.com>
2024-02-20 17:43:47 +0300

    feat:add second cube

--- app/src/main/java/ru/earn1ll/gamecubes/MainActivity.kt
+++ app/src/main/java/ru/earn1ll/gamecubes/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.earn1ll.gamecubes
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -17,9 +18,12 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    @SuppressLint("SetTextI18n")
     private fun rollDice() {
         val dice = Dice(6)
+        val dice2 = Dice(6)
         val roll = dice.roll()
+        val roll2 = dice2.roll()
         when(roll) {
             1->  binding.imDice.setImageResource(R.drawable.dice_one)
             2->  binding.imDice.setImageResource(R.drawable.dice_two)
@@ -28,6 +32,14 @@ class MainActivity : AppCompatActivity() {
             5->  binding.imDice.setImageResource(R.drawable.dice_five)
             6->  binding.imDice.setImageResource(R.drawable.dice_six)
         }
-        binding.tvResult.text = roll.toString()
+        when(roll2) {
+            1->  binding.imDice2.setImageResource(R.drawable.dice_one)
+            2->  binding.imDice2.setImageResource(R.drawable.dice_two)
+            3->  binding.imDice2.setImageResource(R.drawable.dice_three)
+            4->  binding.imDice2.setImageResource(R.drawable.dice_four)
+            5->  binding.imDice2.setImageResource(R.drawable.dice_five)
+            6->  binding.imDice2.setImageResource(R.drawable.dice_six)
+        }
+        binding.tvResult.text = (roll + roll2).toString()
     }
 }
